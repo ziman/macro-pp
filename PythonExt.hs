3424037d40e6299bcc8ecedfbee3944993d52bd5
Matus Tejiscak <ziman@functor.sk>
2018-04-03 13:23:27 +0200

    Add layout.

--- PythonExt.hs
+++ PythonExt.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module PythonExt
     ( Macro(..)
     , macro
@@ -7,6 +9,9 @@ import Parser
 import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.Combinator
+
+import Data.Monoid
+import qualified Data.Text as T
 
 data Macro
     = MacroTest Int
@@ -21,3 +26,16 @@ macroTest = do
 
 macro :: Parser Macro
 macro = macroTest
+
+macroToLines :: Macro -> [T.Text]
+macroToLines = error "not implemented"
+
+indentation :: T.Text -> T.Text
+indentation = fst . T.span (== ' ')
+
+layout :: T.Text -> [Block Macro] -> [T.Text]
+layout ind [] = []
+layout ind (Verbatim t : bs) = t : layout (indentation $ T.reverse t) bs
+layout ind (Macro m : bs) = foldr ((++) . fmtLine) (layout ind bs) (macroToLines m)
+  where
+    fmtLine line = [ind, line, "\n"]
